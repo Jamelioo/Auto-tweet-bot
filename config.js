010efa546fd32a6d4bb63df0c28f805d144e6bf7
Jamelioo <106258063+Jamelioo@users.noreply.github.com>
2022-11-26 18:01:58 -0500

    Autotweet

---
+++ config.js
@@ -0,0 +1,10 @@
+var twit = require('twit');
+var config = require('./config.js');
+
+
+    module.exports ={
+        consumer_key : "1cS1UTa3hTYNeo8ePktVm5iU1",
+        consumer_secret: "SrgwSmvs9XfvgbRGl3l0VWeRKWLuqzOlBMq16JaTidxpi04GJZ",
+        access_token: "1217279091675684865-JWouIndrbat49MonkM24UREFw6IdTX",
+        access_token_secret :"iNG80Kqzi0N9bzRHRiwiYf8YC3MlKz3UOGx0eF32UYTEN"
+    };
